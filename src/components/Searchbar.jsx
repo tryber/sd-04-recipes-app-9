a3828de7691878594478756efecd25264efa3435
juniomelos <juniomelos@gmail.com>
2020-07-31 19:20:12 -0300

    Caixa da search Bar aparecer

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
 
 function SearchBar() {
+
   return (
     <div>
       <input
         type="text"
         data-testid="search-input"
         placeholder="Buscar Receita"
-        value={}
-        onChange={}
+        value={''}
+        onChange={() => {}}
       />
       <label htmlFor="ingredientes">
         <input
           type="radio"
           id="ingredientes"
           data-testid="ingredient-search-radio"
-          value={}
-          onClick={}
+          value={''}
+          onClick={() => {}}
         />
         Ingredientes
       </label>
       <label htmlFor="name">
-        <input type="radio" id="name" data-testid="name-search-radio" value={} onClick={} />
+        <input
+          type="radio"
+          id="name"
+          data-testid="name-search-radio"
+          value={''}
+          onClick={() => {}}
+        />
         Nome
       </label>
       <label>
@@ -29,12 +36,12 @@ function SearchBar() {
           type="radio"
           id="first-letter"
           data-testid="first-letter-search-radio"
-          value={}
-          onClick={}
+          value={''}
+          onClick={() => {}}
         />
         Primeira letra
       </label>
-      <button data-testid="exec-search-btn" type="button" onClick={}>
+      <button data-testid="exec-search-btn" type="button" onClick={() => {}}>
         Buscar
       </button>
     </div>
