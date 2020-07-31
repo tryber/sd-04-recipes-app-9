d797244b632642109e8ba8d5215f1bfac87a21d2
Orlando <programadorlando@gmail.com>
2020-07-31 15:08:39 -0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Login from './pages/Login';
-import NotFound from './pages/NotFound';
-import MainRecipes from './pages/MainRecipes';
+import Routes from './routes/Routes';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route
-          exact path="/comidas"
-          component={MainRecipes}
-        />
-        <Route exact path="/" component={Login} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
+    <Routes />
   );
 }
 
