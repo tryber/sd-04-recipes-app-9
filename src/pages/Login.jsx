48044dc63d441f8669fe24a21cfa98c0b367d344
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-07-31 11:07:14 -0300

    funções da Page Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-  const toSubmit = () => {
+  const [state, setState] = useState({ email: '', password: '' });
+  const { email, password } = state;
+
+  const toSubmit = (email) => {
     localStorage.setItem('mealsToken', 1);
     localStorage.setItem('cocktailsToken', 1);
-    localStorage.setItem('user', { email });
+    localStorage.setItem('user', JSON.stringify({ email }));
+  };
+
+  const checkEmail = (email) => email.match(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/i);
+  const checkPassword = (password) => password.length > 6;
+
+  const toHandleEmail = (email) => {
+    setState({
+      ...state,
+      email: email,
+    });
+  };
+
+  const toHandlePassword = (password) => {
+    setState({
+      ...state,
+      password,
+    });
   };
 
   return (
@@ -13,24 +33,26 @@ const Login = () => {
       <h1>Login</h1>
       <input
         data-testid="email-input"
+        value={email}
         type="email"
         placeholder="Email"
         required
-        onChange={(e) => nomeFunc(e.target.value)}
+        onChange={(e) => toHandleEmail(e.target.value)}
       />
       <input
         data-testid="password-input"
+        value={password}
         type="password"
         placeholder="Senha"
         required
-        onChange={(e) => nomeFunc(e.target.value)}
+        onChange={(e) => toHandlePassword(e.target.value)}
       />
       <Link to="/comidas">
         <button
           data-testid="login-submit-btn"
           type="submit"
-          disabled={nomeFunc()}
-          onClick={() => toSubmit()}
+          disabled={!(checkPassword(password) && checkEmail(email))}
+          onClick={() => toSubmit(email)}
         >
           Entrar
         </button>
