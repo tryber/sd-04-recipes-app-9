7e52143dfa062da0f4252a935095774b6f918a6d
juniomelos <juniomelos@gmail.com>
2020-07-31 16:50:34 -0300

    Header e Footer - CC01

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
-const Header = ({ title }) => {
+const Header = (/* { title } */) => {
   return (
     <div className="row">
       <header className="brand-logo center">
         <Link to="/perfil">
           <img src={profileIcon} alt="profileIcon" data-testid="profile-top-btn" />
         </Link>
-        <h1 data-testid="page-title">{title}</h1>
+        <h1 data-testid="page-title">{/* title */}</h1>
         <input src={searchIcon} type="image" data-testid="search-top-btn" />
       </header>
     </div>
