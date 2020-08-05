941d352e89ad29ffe485a2b3c3bd90880f56d8a6
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-08-05 15:20:46 -0300

    profile

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Favorites = () => (
+  <div>
+    <Header title="Receitas Favoritas" />
+  </div>
+);
+
+export default Favorites;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
 
-const Profile = () => <h1>Profile</h1>;
+const Profile = () => {
+  const emailUser = JSON.parse(localStorage.getItem('user'));
+  return (
+    <div>
+      <Header title="Perfil" />
+      <div data-testid="profile-email">{emailUser}</div>
+      <Link to="/receitas-feitas">
+        <button data-testid="profile-email" type="button">
+          Receitas Feitas
+        </button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button data-testid="profile-favorite-btn" type="button">
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/">
+        <button data-testid="profile-logout-btn" type="button">
+          Sair
+        </button>
+      </Link>
+      <Footer />
+    </div>
+  );
+};
 
 export default Profile;
