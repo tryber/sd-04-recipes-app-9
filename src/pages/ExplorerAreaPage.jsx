fa4ced9998c6c07eb7de28e60dcff44eb564c603
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-08-05 12:11:51 -0300

    fixing cc

--- src/pages/ExplorerAreaPage.jsx
+++ src/pages/ExplorerAreaPage.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-const ExplorerAreaPage = () => {
+const ExplorerAreaPage = () => (
   <div>
     <Header title="Explorar Origem" />
     <Footer />
-  </div>;
-};
+  </div>
+);
 
 export default ExplorerAreaPage;
--- src/pages/ExplorerIngredientsPage.jsx
+++ src/pages/ExplorerIngredientsPage.jsx
@@ -3,10 +3,10 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const ExplorerIngredientsPage = () => (
-    <div>
-      <Header title="Explorar Ingredientes" />
-      <Footer />
-    </div>
+  <div>
+    <Header title="Explorar Ingredientes" />
+    <Footer />
+  </div>
 );
 
 export default ExplorerIngredientsPage;
