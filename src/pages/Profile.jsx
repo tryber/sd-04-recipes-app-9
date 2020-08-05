4ae22c2d9bc8850774c40d515f789bf26f98a752
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-08-05 15:23:06 -0300

    profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { Link } from 'react-router-dom';
 
 const Profile = () => {
   const emailUser = JSON.parse(localStorage.getItem('user'));
