2c9ff215ab2acb711ad0a3f29e6940c42890dfe3
juniomelos <juniomelos@gmail.com>
2020-08-07 10:46:51 -0300

    teste Login

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('Farewell, front-end', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/TRYBE/i);
+  const linkElement = getByText(/LOGIN/i);
   expect(linkElement).toBeInTheDocument();
 });
