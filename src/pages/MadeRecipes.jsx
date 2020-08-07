1580ce8f2301dc6441a13d095d7a5938c8998d5b
Orlando <programadorlando@gmail.com>
2020-08-07 17:40:46 -0300

    Receitas Feitas

--- src/pages/MadeRecipes.jsx
+++ src/pages/MadeRecipes.jsx
@@ -1,10 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import SavedRecipes from '../components/SavedRecipes';
 
-const MadeRecipes = () => (
+const filterButtons = (setFilter) => {
+  const onClick = (event) => {
+    setFilter(event.target.value);
+  };
+  return (
+    <div>
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        value=""
+        onClick={(event) => onClick(event)}
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+        value="comida"
+        onClick={(event) => onClick(event)}
+      >
+        Food
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        value="bebida"
+        onClick={(event) => onClick(event)}
+      >
+        Drinks
+      </button>
+    </div>
+  );
+};
+
+const showRecipes = (filter, recipes, page, setRecipes) => (
   <div>
-    <Header title="Receitas Feitas" />
+    {recipes.filter((recipe) => {
+      if (filter) {
+        return recipe.type === filter;
+      }
+      return true;
+    })
+      .map((recipe, index) => (
+        <SavedRecipes
+          index={index}
+          recipe={recipe}
+          recipes={recipes}
+          page={page}
+          setRecipes={setRecipes}
+        />
+      ))}
   </div>
 );
+
+const MadeRecipes = ({ title, page }) => {
+  const [filter, setFilter] = useState('');
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    if (JSON.parse(localStorage.getItem(page))) {
+      setRecipes(JSON.parse(localStorage.getItem(page)));
+    }
+  }, [page]);
+
+  return (
+    <div>
+      <Header title="Receitas Feitas" />
+      {filterButtons(setFilter)}
+      {showRecipes(filter, recipes, page, setRecipes)}
+    </div>
+  );
+};
+
+MadeRecipes.propTypes = {
+  title: PropTypes.string.isRequired,
+  page: PropTypes.string.isRequired,
+}
 
 export default MadeRecipes;
